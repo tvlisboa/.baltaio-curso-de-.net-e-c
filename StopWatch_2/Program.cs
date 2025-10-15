2c1d71bd3cf5212dda2a2d430418ce81248dc4a0
tvlisboa <tvlisboa@hotmail.com>
2025-10-14 22:55:16 -0300

    novos exercicios com c#

---
+++ StopWatch_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StopWatch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tete stopwatch");
+        }
+        static void Start() 
+        {
+            double time = 10;
+            double currentTime = 0;
+            while ( currentTime != time) 
+            { 
+                Console.WriteLine(time);
+                currentTime++;
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
